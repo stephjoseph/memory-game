f23aa662c4c97c2f901c0f55ceca45b229031da0
Stephen Muñez <sjtmunez@gmail.com>
2024-02-08 10:52:34 +0800

    setup: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "orange-yellow": "#FDA214",
+        "light-steel-blue": "#BCCED9",
+        "dark-slate-blue": "#304859",
+        "midnight-blue": "#152938",
+        "light-grey": "#F2F2F2",
+        "steel-blue": "#7191A5",
+        "sky-blue": "#6395B8",
+        "snow-white": "#FCFCFC",
+      },
+    },
   },
   plugins: [],
 };
