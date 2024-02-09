444c16c3effdfe890658529513a04688e92d1bff
Stephen Muñez <sjtmunez@gmail.com>
2024-02-09 12:27:21 +0800

    setup: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  );
-}
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Root from "./routes/route";
+import Home from "./routes/Home";
+import Game from "./routes/Game";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/game",
+        element: <Game />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
